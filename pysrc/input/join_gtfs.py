6d282e241d0659b38b1765b969a98d37bac1cf47
cryolins <hepeilin07@gmail.com>
2026-05-05 23:21:22 -0400

    modified trip and route length sampling

--- pysrc/input/join_gtfs.py
+++ pysrc/input/join_gtfs.py
@@ -17,32 +17,48 @@ delay_df.drop(columns=["Unnamed: 0"], inplace=True)
 delay_df = delay_df[delay_df["route_id"] > 6] # delete any subways that sneaked in
 delay_df.info()
 
-# get sample trip per route from trips data
-trip_by_route_df = (
-    pd.read_csv(DATA_DIR_PATH / "trips.csv")
+# trips data unaggregated
+trips_df: pd.DataFrame = (
+    pd.read_csv(DATA_DIR_PATH / "trips.csv", usecols=["route_id", "service_id", "trip_id"])
     .loc[lambda df: df["service_id"] < 10]
-    .groupby("route_id")
-    .agg({
-        "trip_id": "last",
-        #"shape_id": "last"
-    })
+    .drop(columns="service_id")
 )
-df = pd.merge(delay_df, trip_by_route_df, on="route_id", how="inner")
-df.info()
 
 # get distances from gtfs data
 DOWNTOWN_POINT = [43.65472, -79.38827] # dundas and university, decent centre for downtown
 stops_df = pd.read_csv(DATA_DIR_PATH / "stops.csv", usecols=["stop_id", "stop_lat", "stop_lon"])
 stop_times_df = (
     pd.merge(pd.read_csv(DATA_DIR_PATH / "stop_times.csv", usecols=["trip_id", "stop_id", "stop_sequence", "shape_dist_traveled"]),
-             df["trip_id"].drop_duplicates(), how="inner", on="trip_id")  
+             trips_df["trip_id"].drop_duplicates(), how="inner", on="trip_id")  
 ) # immediately filter out unnecessary data to reduce memory
+
+# step to select a trip for a route based on 80th percentile in length
+# ensures something decently long but probably not an outlier
+def get_percentile_row(group, q=0.8):
+    idx = int(q * len(group)) - 1
+    return group.iloc[idx]
+
+select_trip_df = (
+    pd.merge(stop_times_df, trips_df, how="inner", on="trip_id")
+    .groupby("trip_id")
+    .agg(trip_length=("shape_dist_traveled", "max"), route_id=("route_id", "first"))
+    .reset_index()
+    .sort_values("trip_length")
+    .groupby("route_id")
+    .apply(get_percentile_row)
+    .reset_index()
+)
+
+# now merge stop_times with selected trips, so even less trips saved and memory used
+stop_times_df = pd.merge(stop_times_df, select_trip_df["trip_id"].drop_duplicates(), how="inner", on="trip_id")
+# merge delay df with select trip df to supply it with trip_id and route length information
+df = pd.merge(delay_df, select_trip_df, on="route_id", how="inner")
+df.info()
 
 route_trip_df = pd.merge(stop_times_df, stops_df, on="stop_id", how="inner")
 route_trip_df["dt_dists"] = haversine_distances(np.radians(route_trip_df[["stop_lat", "stop_lon"]]), [np.radians(DOWNTOWN_POINT)]) * 6371
 route_trip_df = route_trip_df.groupby("trip_id").agg(
     route_stops=("stop_sequence", "max"),
-    route_length=("shape_dist_traveled", "max"),
     dt_dist_mean=("dt_dists", "mean"),
     dt_dist_std=("dt_dists", "std")
 )
@@ -57,6 +73,7 @@ df = (
         "Direction": "direction",
         "Date": "date",
         "Min Delay": "min_delay",
+        "trip_length": "route_length"
     })
 )
 df.info()
